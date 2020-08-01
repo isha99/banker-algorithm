deb5de4d5f43509d44dd616ff6446c0917ca3f7c
isha99 <56374929+isha99@users.noreply.github.com>
2020-07-31 21:43:19 -0400

    Add files via upload

---
+++ a4_bankeralgo.c
@@ -0,0 +1,24 @@
+/**
+* Project: Banker's algorithm
+* author: Isha Raulji 
+**/
+
+#include <stdio.h>
+
+//main will ask for 
+int main(int argc, char** argv){
+
+
+
+
+}
+// this is to determine whether a safe sequence is found
+is_safe_seq(){
+
+}
+
+//this is to run the program with the given processes and resources
+run_program()
+
+
+
